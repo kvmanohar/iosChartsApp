2a0e078a118f2431e7453b18ce6c7e5805a605d4
Manohar Kurapati <kvmanohar@gmail.com>
2018-01-21 14:21:40 +0000

    Updated the look of the chart. Removed the left and right axis and grid lines

--- iosChartsApp/ViewController.swift
+++ iosChartsApp/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0]
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 5.0, 10.0]
         
         setChart(dataPoint: months, values: unitsSold)
     
@@ -46,24 +46,38 @@ class ViewController: UIViewController {
             dataEntries.append(dataEntry)
         }
         
-        let ds1 = BarChartDataSet(values: dataEntries, label: "Months")
+        let ds1 = BarChartDataSet(values: dataEntries, label: nil)
         
         data.addDataSet(ds1)
         barChartView.data = data
         
-        //Formating the chart
+        
+        
+        // Formating the chart
+        // https://stackoverflow.com/questions/34518228/hide-bottom-x-axis-in-horizontal-bar-chart
+        
         barChartView.noDataText = "You have not practised any words in last 10 days."
         barChartView.noDataTextColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        barChartView.chartDescription?.text = ""
+        barChartView.pinchZoomEnabled = false
+        barChartView.drawGridBackgroundEnabled = false
+        barChartView.legend.enabled = false
+        
         barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:dataPoint)
         
-        
         barChartView.xAxis.granularity = 1
-        barChartView.xAxis.centerAxisLabelsEnabled = true
+        barChartView.xAxis.centerAxisLabelsEnabled = false
         barChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        barChartView.xAxis.drawGridLinesEnabled = false
         
         barChartView.leftAxis.drawGridLinesEnabled = false
+        barChartView.leftAxis.drawLabelsEnabled = false
+        barChartView.leftAxis.drawAxisLineEnabled = false
+        
         barChartView.rightAxis.drawGridLinesEnabled = false
-        barChartView.xAxis.drawGridLinesEnabled = false
+        barChartView.rightAxis.drawLabelsEnabled = false
+        barChartView.rightAxis.drawAxisLineEnabled = false
+        
         
         barChartView.fitBars = true
         
